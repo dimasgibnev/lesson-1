45c7e51762c99497c1142a53480cc4000461e2cc
dimasgibnev <153992789+dimasgibnev@users.noreply.github.com>
2023-12-25 06:33:36 +0700

    update lesson-1

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Создатель JavaScript - Брендан Эйх.');
+alert('Создан за 10 дней на основе синтаксиса Java.');
+alert('Анонсирован 18 июля 1996 года.');
+alert('JavaScript - это не Java');
+alert('ECMAScript');
