f86f2fd6371d4b0dbfc626fa1b31b66af6e8aae2
dimasgibnev <153992789+dimasgibnev@users.noreply.github.com>
2023-12-25 06:30:08 +0700

    Update js-info.js

--- first-project/js-info.js
+++ first-project/js-info.js
@@ -1,5 +1,5 @@
 alert('Создатель JavaScript - Брендан Эйх.');
-alert('Создан за 10 дней на основе Java.');
+alert('Создан за 10 дней на основе синтаксиса Java.');
 alert('Анонсирован 18 июля 1996 года.');
 alert('JavaScript - это не Java');
 alert('ECMAScript');
